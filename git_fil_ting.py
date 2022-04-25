8777d9dc466230971fb0901b06a9e669b4567f53
stinasb <stinabogsti@gmail.com>
2022-04-25 10:16:28 +0200

    Nye Målinger

--- git_fil_ting.py
+++ git_fil_ting.py
@@ -3,9 +3,11 @@ import os
 def run():
     email = "stinabogsti@gmail.com"
     name = "stinasb"
+    address = "https://github.com/stinasb/snowpilot.git"
     SendeMappe= '/home/pi/Documents/SendeMappe'
     os.system(f'git config --global user.email {email}')
     os.system(f'git config --global user.name {name}')
+    os.system(f'git remote add origin {address}')
     os.system(f'cd {SendeMappe} && git add . && git commit -m "Nye Målinger" && git pull && git push')
     
     print("program ended succesfully")
