444d241da73556669cc21bbc87b4e4f624f658ad
stinasb <stinabogsti@gmail.com>
2022-04-25 10:14:22 +0200

    Nye Målinger

---
+++ git_fil_ting.py
@@ -0,0 +1,13 @@
+import os
+
+def run():
+    email = "stinabogsti@gmail.com"
+    name = "stinasb"
+    SendeMappe= '/home/pi/Documents/SendeMappe'
+    os.system(f'git config --global user.email {email}')
+    os.system(f'git config --global user.name {name}')
+    os.system(f'cd {SendeMappe} && git add . && git commit -m "Nye Målinger" && git pull && git push')
+    
+    print("program ended succesfully")
+
+run()
